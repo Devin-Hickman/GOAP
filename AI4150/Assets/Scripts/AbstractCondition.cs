cffcb227d65663cc1efa06dce058cc9b1500389e
Devin H <dhickman811@gmail.com>
2019-03-26 04:09:35 -0400

    GOAP Start

---
+++ AI4150/Assets/Scripts/AbstractCondition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractCondition 
+{
+    private bool completion = false;
+    public bool IsCompleted()
+    {
+        return completion;
+    }
+
+    public void Complete()
+    {
+        completion = true;
+    }
+}
