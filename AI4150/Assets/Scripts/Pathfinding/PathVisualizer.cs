3e7aead660d663d2b020f59f19aef5cac357414d
Devin H <dhickman811@gmail.com>
2019-04-09 16:47:13 -0400

    Line renderer working

--- AI4150/Assets/Scripts/Pathfinding/PathVisualizer.cs
+++ AI4150/Assets/Scripts/Pathfinding/PathVisualizer.cs
@@ -5,23 +5,26 @@ using UnityEngine;
 public class PathVisualizer : MonoBehaviour
 {
     LineRenderer lineRenderer;
+    [SerializeField]
+    List<Vector3> positions = new List<Vector3>();
 
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.startColor = Color.red;
         lineRenderer.endColor = Color.red;
+        lineRenderer.startWidth = .25f;
+        lineRenderer.endWidth = .25f;
     }
 
     public void DrawLines(List<PathFindingNode> nodes)
     {
-        List<Vector3> positions = new List<Vector3>();
-
+        positions = new List<Vector3>();
         foreach(PathFindingNode node in nodes)
         {
             positions.Add(new Vector3(node.GetX, node.GetY, 1));
         }
-
+        lineRenderer.positionCount = positions.Count;
         lineRenderer.SetPositions(positions.ToArray());
     }
 }
